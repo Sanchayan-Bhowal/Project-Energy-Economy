267d83adde2552db6bab862ee54e43b03961f960
AprameyaGH <91447184+AprameyaGH@users.noreply.github.com>
2021-11-14 19:03:59 +0530

    histogram display name

--- reproduce.r
+++ reproduce.r
@@ -1,3 +1,4 @@
+#Reproduction of page 7,8 in data paper
 data <- read.csv("data.csv")
 library(timeDate)
 describe <- function(variable) {
@@ -25,12 +26,14 @@ result <- rbind(
 
 print(result)
 
+#We can add Normality check for the data set. qq plots.
+
 windows()
 par(mfrow = c(2, 2))
-hist(data$ln_price_1, freq = F, breaks = 30, xlim = range(c(8, 16)))
-hist(data$ln_price_2, freq = F, breaks = 30, xlim = range(c(10, 16)))
-hist(data$perc_change_p2_to_p1, freq = F, breaks = 30)
-hist(data$days_between_sale, freq = F, breaks = 35)
+hist(data$ln_price_1, freq = F, breaks = 30, xlim = range(c(8, 16)),main="Natural logarithm of first transaction price")
+hist(data$ln_price_2, freq = F, breaks = 30, xlim = range(c(10, 16)),main="Natural logarithm of second transaction price")
+hist(data$perc_change_p2_to_p1, freq = F, breaks = 30,main="Percentual price change between transactions")
+hist(data$days_between_sale, freq = F, breaks = 35,main="Period of time between both transactions")
 
 epc <- function(x) {
     f <- as.numeric(table(x)[2])
@@ -58,5 +61,5 @@ print(epc_band)
 
 windows()
 par(mfrow = c(1, 2))
-hist(data$epc_100, freq = F)
+hist(data$epc_100, freq = F,main="EPC standard assessment proceducre(SAP) points")
 boxplot(data$epc_100)
