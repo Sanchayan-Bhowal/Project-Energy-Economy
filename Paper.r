ae9cc55678a1f37997dd89b0af90ef57d40289a7
Sanchayan-Bhowal <sanchayan.bhowal2509@gmail.com>
2021-11-18 19:40:26 +0530

    Better code implemented

--- Paper.r
+++ Paper.r
@@ -1,21 +1,15 @@
 data <- read.csv("data.csv")
 
-epc_price_1 <- c(0)
-epc_price_1[1] <- mean(data$price_1[data$epc_rating_b == 1])
-epc_price_1[2] <- mean(data$price_1[data$epc_rating_c == 1])
-epc_price_1[3] <- mean(data$price_1[data$epc_rating_d == 1])
-epc_price_1[4] <- mean(data$price_1[data$epc_rating_e == 1])
-epc_price_1[5] <- mean(data$price_1[data$epc_rating_f == 1])
-epc_price_1[6] <- mean(data$price_1[data$epc_rating_g == 1])
-epc_price_2 <- c(0)
-epc_price_2[1] <- mean(data$price_2[data$epc_rating_b == 1])
-epc_price_2[2] <- mean(data$price_2[data$epc_rating_c == 1])
-epc_price_2[3] <- mean(data$price_2[data$epc_rating_d == 1])
-epc_price_2[4] <- mean(data$price_2[data$epc_rating_e == 1])
-epc_price_2[5] <- mean(data$price_2[data$epc_rating_f == 1])
-epc_price_2[6] <- mean(data$price_2[data$epc_rating_g == 1])
+epc_rating <- data$epc_rating_a +
+    2 * data$epc_rating_b +
+    3 * data$epc_rating_c +
+    4 * data$epc_rating_d +
+    5 * data$epc_rating_e + 6 * data$epc_rating_f + 7 * data$epc_rating_g
 
 barplot(
-    rbind(epc_price_1, epc_price_2),
+    rbind(
+        tapply(data$price_1, epc_rating, mean),
+        tapply(data$price_2, epc_rating, mean)
+    ),
     beside = TRUE, names.arg = c("B", "C", "D", "E", "F", "G")
 )
