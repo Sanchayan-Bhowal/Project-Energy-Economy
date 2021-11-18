01996207dedec3be5c8b457cc086ddc254aff2ae
Sanchayan-Bhowal <sanchayan.bhowal2509@gmail.com>
2021-11-18 20:03:08 +0530

    price changed to ln(price)

--- Paper.r
+++ Paper.r
@@ -6,10 +6,17 @@ epc_rating <- data$epc_rating_a +
     4 * data$epc_rating_d +
     5 * data$epc_rating_e + 6 * data$epc_rating_f + 7 * data$epc_rating_g
 
+windows()
 barplot(
     rbind(
-        tapply(data$price_1, epc_rating, mean),
-        tapply(data$price_2, epc_rating, mean)
+        tapply(data$ln_price_1, epc_rating, mean),
+        tapply(data$ln_price_2, epc_rating, mean)
     ),
-    beside = TRUE, names.arg = c("B", "C", "D", "E", "F", "G")
+    beside = TRUE, ylim = c(0, 12),
+    names.arg = c("B", "C", "D", "E", "F", "G")
 )
+
+windows()
+par(mfrow = c(1, 2))
+boxplot(data$ln_price_1 ~ epc_rating)
+boxplot(data$ln_price_2 ~ epc_rating)
